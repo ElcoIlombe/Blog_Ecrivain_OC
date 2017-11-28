d136caee3221ebb674aa79b19d9ecdd8f64e5ea1
Jonathan <jonathan.ilombe@hotmail.com>
2017-11-28 17:22:57 +0100

    Création des méthod permettant de vérifier si l'url correspond à une route

--- Core/Router.php
+++ Core/Router.php
@@ -12,6 +12,13 @@ class Router
 	protected $routes = [];
 
 	/**
+	*Parameters from the matched route
+	*@var array
+	*/
+
+	protected $params = [];
+
+	/**
 	* Add a route to the routing table
 	*
 	*@param string $route -> The route URL
@@ -31,6 +38,36 @@ class Router
 	public function getRoutes()
 	{
 		return $this->routes;
+	}
+
+	/*
+	* Match the route to the routes in the routing table, setting the $params
+	*property if a route is found.
+	*
+	*@param string $url The route URL
+	*
+	*@return boolean true if a match found, false otherwise
+	*/
+	public function match($url)
+	{
+		foreach($this->routes as $route => $params)
+		{
+			if ($url == $route)
+			{
+				$this->params = $params;
+				return true;
+			}
+		}
+		return false;
+	}
+	/**
+	*Get the currently matched parameters
+	*
+	*@return array
+	*/
+	public function getParams()
+	{
+		return $this->params;
 	}
 
 }
--- public/index.php
+++ public/index.php
@@ -10,4 +10,14 @@ require "../Core/Router.php";
 
 $router = new Router();
 
-echo get_class($router);
+//echo get_class($router);
+// Add the routes
+$router->add('', ['controller'=>'Home', 'action' =>'index']);
+$router->add('posts', ['controller'=>'Posts', 'action' =>'index']);
+$router->add('posts', ['controller'=>'Posts', 'action' =>'new']);
+
+//Display the routing table 
+echo '<pre>';
+var_dump($router->getRoutes());
+echo '</pre>';
+
