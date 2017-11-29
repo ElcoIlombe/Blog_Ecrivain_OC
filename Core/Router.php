e86186a753fe7c78ef11af7b7aa452a04ecb727e
Jonathan <jonathan.ilombe@hotmail.com>
2017-11-29 15:37:08 +0100

    Création de la method permettant de dispatcher les requêtes vers les bons controller

--- Core/Router.php
+++ Core/Router.php
@@ -1,98 +1,155 @@
-<?php 
-/* 
-* Router
-*/
+<?php
 
+/**
+ * Router
+ *
+ * PHP version 5.4
+ */
 class Router
 {
-	/**
-	* Associative arraay of routes (the routing table)
-	*@var array
-	*/
-	protected $routes = [];
 
-	/**
-	*Parameters from the matched route
-	*@var array
-	*/
+    /**
+     * Associative array of routes (the routing table)
+     * @var array
+     */
+    protected $routes = [];
 
-	protected $params = [];
+    /**
+     * Parameters from the matched route
+     * @var array
+     */
+    protected $params = [];
 
-	/**
-	* Add a route to the routing table
-	*
-	*@param string $route -> The route URL
-	*@param array $param -> Parameters (controller,acion,etc)
-	* 
-	*@return void
-	*/
-	public function add($route,$params = [])
-	{
-		//Convert the route to a regular expression : escape forward slashes
-		$route = preg_replace('/\//', '\\/', $route);
+    /**
+     * Add a route to the routing table
+     *
+     * @param string $route  The route URL
+     * @param array  $params Parameters (controller, action, etc.)
+     *
+     * @return void
+     */
+    public function add($route, $params = [])
+    {
+        // Convert the route to a regular expression: escape forward slashes
+        $route = preg_replace('/\//', '\\/', $route);
 
-		// Convert variable e.g. {controller}
-		$route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
-		//Add start and end delimiters and case insensitive flag
+        // Convert variables e.g. {controller}
+        $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
 
-		$route = '/^' . $route . '$/i';
+        // Convert variables with custom regular expressions e.g. {id:\d+}
+        $route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
 
-		$this->routes[$route] = $params ;
-	}
+        // Add start and end delimiters, and case insensitive flag
+        $route = '/^' . $route . '$/i';
 
-	/* Get all the routes from the routing table
-	* 
-	*@return array
-	*/
-	public function getRoutes()
-	{
-		return $this->routes;
-	}
+        $this->routes[$route] = $params;
+    }
 
-	/*
-	* Match the route to the routes in the routing table, setting the $params
-	*property if a route is found.
-	*
-	*@param string $url The route URL
-	*
-	*@return boolean true if a match found, false otherwise
-	*/
-	public function match($url)
-	{
-		// Match to the fixed URL format /controller/action
-		//$reg_exp = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/";
-		
-		foreach($this->routes as $route => $params)
-		{
-		if (preg_match($route, $url, $matches))
-			{
-				//Get named capture group values
-				//$params = [];
+    /**
+     * Get all the routes from the routing table
+     *
+     * @return array
+     */
+    public function getRoutes()
+    {
+        return $this->routes;
+    }
 
-				foreach ($matches as $key => $match) {
-					if (is_string($key))
-						{
-							$params[$key] = $match;
-						}
-					$this->params =$params;
-					return true;
-				}
-			}
-		}
-		
-		return false;
+    /**
+     * Match the route to the routes in the routing table, setting the $params
+     * property if a route is found.
+     *
+     * @param string $url The route URL
+     *
+     * @return boolean  true if a match found, false otherwise
+     */
+    public function match($url)
+    {
+        foreach ($this->routes as $route => $params) {
+            if (preg_match($route, $url, $matches)) {
+                foreach ($matches as $key => $match) {
+                    if (is_string($key)) {
+                        $params[$key] = $match;
+                    }
+                }
 
+                $this->params = $params;
+                return true;
+            }
+        }
 
+        return false;
+    }
 
-	}
-	/**
-	*Get the currently matched parameters
-	*
-	*@return array
-	*/
-	public function getParams()
-	{
-		return $this->params;
-	}
+    /**
+     * Get the currently matched parameters
+     *
+     * @return array
+     */
+    public function getParams()
+    {
+        return $this->params;
+    }
 
+    /**
+    * Dispatch the route, creating the controller object and running the action method
+    *@param string $url The route URL
+    *
+    *@return void
+    */
+    public function dispatch($url)
+    {
+        if($this->match($url))
+              {
+                $controller = $this->params['controller'];
+                $controller = $this->converToStudlyCaps($controller);
+                if(class_exists($controller))
+                {
+                    $controller_object = new $controller();
+
+                    $action = $this->params['action'];
+                    $action = $this->convertToCamelCase($action);
+                    if(is_callable([$controller_object, $action]))
+                    {
+                        $controller_object->$action();
+                    } else 
+                    {
+                        echo 'Method '.$action .' (in controller '.$controller. ') not found';
+                    }
+                }
+                else 
+                {
+                    echo 'Controller class ' .$controller. ' not found';
+                }
+              }
+              else {
+                echo 'No route matched.';
+              }
+    }
+
+    /**
+    *Convert the string with hyphens to StudlyCaps,
+    *e.g post->authors => PostAuthors
+    *
+    *@param string $string The string to convert
+    *
+    *@return string
+    */
+
+    protected function converToStudlyCaps($string)
+    {
+        return str_replace(' ', '', ucwords(str_replace('-', ' ', $string)));
+    }
+    /**
+    * Convert the string with hyphens to camelCase,
+    *e.g add-new => addNew
+    *
+    *@param string $string The string to convert
+    *
+    *@return string
+    */
+    protected function convertToCamelCase($string)
+    {
+        return lcfirst($this->converToStudlyCaps($string));
+    }
 }
--- public/index.php
+++ public/index.php
@@ -1,39 +1,44 @@
-<?php 
+<?php
 
-/** Front Controller
-*
-*/
+/**
+ * Front controller
+ *
+ */
 
-//echo 'Requested URL = "'.$_SERVER['QUERY_STRING'].'"';
-
-require "../Core/Router.php";
+/**
+ * Routing
+ */
+require '../Core/Router.php';
+/**
+ * Require the controller class
+ */
+require '../App/Controllers/Posts.php';
 
 $router = new Router();
 
-//echo get_class($router);
 // Add the routes
-$router->add('', ['controller'=>'Home', 'action' =>'index']);
-$router->add('posts', ['controller'=>'Posts', 'action' =>'index']);
-//$router->add('posts', ['controller'=>'Posts', 'action' =>'new']);
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
 $router->add('{controller}/{action}');
-$router->add('admin/{controller}/{action}');
-
-//Display the routing table 
-//echo '<pre>';
+$router->add('{controller}/{id:\d+}/{action}');
+/*    
+// Display the routing table
+echo '<pre>';
 //var_dump($router->getRoutes());
-//echo '</pre>';
+echo htmlspecialchars(print_r($router->getRoutes(), true));
+echo '</pre>';
 
 
 // Match the requested route
 $url = $_SERVER['QUERY_STRING'];
 
-if ($router->match($url))
-{
-	echo '<pre>';
-	var_dump($router->getRoutes());
-	echo '</pre>';
-} else 
-{
-	echo "No route found for URL '$url' ";
+if ($router->match($url)) {
+    echo '<pre>';
+    var_dump($router->getParams());
+    echo '</pre>';
+} else {
+    echo "No route found for URL '$url'";
 }
+*/
 
+$router->dispatch($_SERVER['QUERY_STRING']);
