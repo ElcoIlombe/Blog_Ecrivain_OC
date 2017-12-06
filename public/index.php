8c13a59b27904e1714f8f834162db573cffe9b52
Jonathan <jonathan.ilombe@hotmail.com>
2017-12-06 12:50:18 +0100

    Ajout de composer et passages des différents autoloader sous composer

--- public/index.php
+++ public/index.php
@@ -5,24 +5,17 @@
  *
  * PHP version 5.4
  */
+
+/**
+*Composer
+*/
+require '../vendor/autoload.php';
+
 /**
 *Twig 
 */
-require_once dirname(__DIR__) . "/vendor/Twig/lib/Twig/Autoloader.php";
+
 Twig_Autoloader::register();
-
-
-/**
- * Autoloader
- */
-spl_autoload_register(function ($class) {
-    $root = dirname(__DIR__);   // get the parent directory
-    $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
-    if (is_readable($file)) {
-        require $root . '/' . str_replace('\\', '/', $class) . '.php';
-    }
-});
-
 
 /**
  * Routing
