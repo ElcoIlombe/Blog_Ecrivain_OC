1e073297c3b3c2161d4b912a2cba42885da20b24
Jonathan <jonathan.ilombe@hotmail.com>
2017-12-06 14:12:27 +0100

    Mise en place de la connexion à la BDD, création des models

--- App/Controllers/Posts.php
+++ App/Controllers/Posts.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use Core\View;
 
+use App\Models\Post;
 /**
  * Posts controller
  *
@@ -19,7 +20,11 @@ class Posts extends \Core\Controller
      */
     public function indexAction()
     {
-        View::renderTemplate('Posts/index.html');
+        $posts= Post::getAll();
+        View::renderTemplate('Posts/index.html', 
+            [
+                'posts' => $posts
+            ]);
     }
 
     /**
